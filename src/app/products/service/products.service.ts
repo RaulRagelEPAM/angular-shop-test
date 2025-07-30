a58c3a60673943f1e665321b77966e0637f531bd
RaulRagel <raul.rs3006@gmail.com>
2025-07-30 17:11:12 +0200

    Probando otras apis

--- src/app/products/service/products.service.ts
+++ src/app/products/service/products.service.ts
@@ -14,16 +14,25 @@ export class ProductService {
 
   constructor(private http: HttpClient) { }
 
-  // API: https://fakestoreapi.com/docs
+  // ** API 1: https://fakestoreapi.com/docs
   private productsURL: string = 'https://fakestoreapi.com/products';
+  // ** Local JSON: 'server/db.json'
+  // private productsURL: string = 'http://localhost:3000/products';
+  // ** API 2: https://fakestoreapi.in/docs
+  // private productsURL: string = 'https://fakestoreapi.in/api/products';
 
-  getProducts(): Observable<Product[]> {
-    return this.http.get<Product[]>(this.productsURL);
+  getProducts(): Observable<any> {
+    return this.http.get<Product[]>(this.productsURL); // API 1
     // ** No items test:
     // return of([]).pipe(delay(3000));
     // ** Limit to 10 items test:
     // return this.http.get<Product[]>(this.productsURL).pipe(
     //   map(products => products.slice(0, 3))
+    // );
+
+    // return this.http.get<any>(this.productsURL) // API 2
+    // .pipe(
+    //   map(response => response.products)
     // );
   }
 }
